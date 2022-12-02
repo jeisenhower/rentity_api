ae62f529100d5ee0add879f17de7553484863ad8
jeisenhower <jme00@lvc.edu>
2022-12-02 18:17:42 -0500

    Debugging the new checkAuth function.

--- routes/organizations.js
+++ routes/organizations.js
@@ -70,31 +70,14 @@ const validateEmail = (email) => {
 }*/
 
 async function checkAuth(req, res, next) {
-    // Get the api key and look up the corresponding account with it. Whatever user, if any, it corresponds to, pass the user's account info along by creating
-    // a new organization object within the req object
-    // If user is using api key, pass along the user profile in the passed data object added to the req object. If using access tokens, get the data from the
-    // access token and pass it in the passed data object, as well as set a req.method variable as appropriate (either key or token).
     if (req.headers['x-api-key'] !== undefined) {
-        // Look up the organization and see if the key matches the organization
-        /*let organization = null;
-        if (req.headers['organizationid'] !== undefined) {
-            organization = await organizations.findOne({ organizationId: req.headers['organizationid'] });
-        } else if (req.headers['organization'] !== undefined) {
-            organization = await organizations.findOne({ organization: req.headers['organization'] });
-        } else {
-            return res.status(401).json({
-                error: "Organization name or ID must be provided in addition to the API key in request headers."
-            })
-        }
-        if (organization == null) {
-            return res.status(401).json({
-                error: "Organization does not exist. Access denied."
-            });
-        }*/
+        
 
         // Encrypt the api key and search for the matching api key in the database
         let encryptedAPIKey = hash.encrypt(req.headers['x-api-key']);
         console.log(`Encrypted API key: ${encryptedAPIKey}`);
+        let reDecrypted = hash.decrypt(encryptedAPIKey);
+        console.log(`Re-decrypted api key: ${reDecrypted}`);
 
         const organizations = dbo.getOrganizationsCollection();
         let organization = await organizations.findOne({apiKey: encryptedAPIKey});

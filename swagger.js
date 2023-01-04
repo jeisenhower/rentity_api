8da3136e75fc51fc807d587d8bf0644810527f50
jeisenhower <36710001+jeisenhower@users.noreply.github.com>
2023-01-04 14:47:26 -0500

    Delete swagger.js

--- swagger.js
+++
@@ -1,6 +0,0 @@
-import swaggerAutogen from 'swagger-autogen';
-
-const outputFile = "./swagger_output.json";
-const endpointsFiles = ["./routes/auth.js", "./routes/organizations.js", "./routes/collections.js", "./routes/entities.js"];
-
-swaggerAutogen(outputFile, endpointsFiles);
